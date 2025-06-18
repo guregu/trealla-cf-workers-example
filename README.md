4a5581ecd40564fe5a08fccbacad722cc0c2b9b1
guregu <greg@toki.waseda.jp>
2025-06-18 21:16:19 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Example of using trealla-js in Cloudflare Workers. Adapted from their Hello World example.
+
+## Setup
+
+- Grab `libtpl-js.wasm` from https://github.com/guregu/trealla/releases
+- Move `libtpl-js.wasm` to the `src` folder.
+
+## Try
+
+```console
+npm run dev
+```
--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,16 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { load, Prolog } from "trealla";
+import tpl from "./libtpl-js.wasm";
+
+await load(tpl);
+
+const pl = new Prolog();
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
-		}
+		const answer = await pl.queryOnce(`X = "hello world".`);
+		return new Response(JSON.stringify(answer));
 	},
 } satisfies ExportedHandler<Env>;
